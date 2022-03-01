803da71cebb7e95eb6fd79aada24d6fee12b1627
Federico Mancinelli <federico@studiogusto.com>
2022-03-01 14:24:45 +0100

    modified js files

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite 2022!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <a href="https://vitejs.dev/guide/features.html">Documentation</a>
 `
